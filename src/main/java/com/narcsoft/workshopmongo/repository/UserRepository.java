703f8f530c86427b6ca18987dc3bccf7cd540c26
enarciso2009 <enarciso2009@gmail.com>
2019-09-30 10:06:26 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/narcsoft/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/narcsoft/workshopmongo/repository/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.narcsoft.workshopmongo.domain.User;
-// somente com este comando abaixo é possivel alterar deletar criar sem necessidade de criar uma linha de comando com os querys para isso
+// somente com este comando abaixo é possivel alterar deletar criar sem necessidade de criar uma linha p
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
 	
