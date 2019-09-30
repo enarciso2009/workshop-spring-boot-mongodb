92065f142724521e1fda0af070f45b55b987fa75
enarciso2009 <enarciso2009@gmail.com>
2019-09-30 16:27:39 -0300

    obtendo usuario por id

--- src/main/java/com/narcsoft/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/narcsoft/workshopmongo/repository/UserRepository.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Repository;
 import com.narcsoft.workshopmongo.domain.User;
 // somente com este comando abaixo é possivel alterar deletar criar sem necessidade de criar uma linha p
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
-	
+public interface UserRepository extends MongoRepository<User, String> {
 
-}
+
+
+	
+    
+
+
+	}
--- src/main/java/com/narcsoft/workshopmongo/services/UserService.java
+++ src/main/java/com/narcsoft/workshopmongo/services/UserService.java
@@ -1,21 +1,30 @@
 package com.narcsoft.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.narcsoft.workshopmongo.domain.User;
 import com.narcsoft.workshopmongo.repository.UserRepository;
+import com.narcsoft.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
-		
+
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
 }
