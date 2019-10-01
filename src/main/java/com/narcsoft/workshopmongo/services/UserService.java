4c642159662e6463eead97fcb3d04f9dd06fe009
enarciso2009 <enarciso2009@gmail.com>
2019-10-01 14:13:58 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/narcsoft/workshopmongo/services/UserService.java
+++ src/main/java/com/narcsoft/workshopmongo/services/UserService.java
@@ -31,31 +31,24 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
-	
-	
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
-	
-	//public User update(User obj) {
-	//	User newObj = findById(obj.getId());
-	//	updateData(newObj, obj);
-	//	return repo.save(newObj);
-	//	}
 
-	//private void updateData(User newObj, User obj) {
-		// TODO Auto-generated method stub
-		
-//	}
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
 
-	//public User fromDTO(UserDTO objDto) {
-		// TODO Auto-generated method stub
-	//	return null;
-	//}
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 }
