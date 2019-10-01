a60db34ee4373b6650a47aeee719f12c4c1e14f7
enarciso2009 <enarciso2009@gmail.com>
2019-10-01 16:32:54 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/narcsoft/workshopmongo/config/Instantiation.java
+++ src/main/java/com/narcsoft/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.narcsoft.workshopmongo.domain.Post;
 import com.narcsoft.workshopmongo.domain.User;
+import com.narcsoft.workshopmongo.dto.AuthorDTO;
 import com.narcsoft.workshopmongo.repository.PostRepository;
 import com.narcsoft.workshopmongo.repository.UserRepository;
 
@@ -36,11 +37,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou Viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou Viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+	
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	}
