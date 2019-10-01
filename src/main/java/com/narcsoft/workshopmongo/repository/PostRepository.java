7979d765b1ad52d88f3185541f2d507eea91beae
enarciso2009 <enarciso2009@gmail.com>
2019-10-01 16:21:10 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/narcsoft/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,17 @@
+package com.narcsoft.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.narcsoft.workshopmongo.domain.Post;
+// somente com este comando abaixo é possivel alterar deletar criar sem necessidade de criar uma linha p
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+
+
+	
+    
+
+
+	}
