c7006195554582ab7c0a555c89e4cc37f6d5a26a
enarciso2009 <enarciso2009@gmail.com>
2019-10-02 13:39:55 -0300

    Consulta simples com @Query

--- src/main/java/com/narcsoft/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/narcsoft/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.narcsoft.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.narcsoft.workshopmongo.domain.Post;
@@ -16,6 +17,10 @@ import com.narcsoft.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	
 	List<Post> findByTitleContainingIgnoreCase(String Text); // monta uma consulta automaticamente na base de dados para localizar um texto no banco de dados...
 	
 	
