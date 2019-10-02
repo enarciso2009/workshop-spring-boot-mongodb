a2e374b31198c7730295807becc055f32db8cb6b
enarciso2009 <enarciso2009@gmail.com>
2019-10-02 13:14:20 -0300

    Consulta simples com Query methods

--- src/main/java/com/narcsoft/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/narcsoft/workshopmongo/repository/PostRepository.java
@@ -1,17 +1,23 @@
 package com.narcsoft.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.narcsoft.workshopmongo.domain.Post;
+
+
+
+
 // somente com este comando abaixo é possivel alterar deletar criar sem necessidade de criar uma linha p
+
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-
-
+	List<Post> findByTitleContainingIgnoreCase(String Text); // monta uma consulta automaticamente na base de dados para localizar um texto no banco de dados...
 	
-    
-
-
+	
+	
 	}
