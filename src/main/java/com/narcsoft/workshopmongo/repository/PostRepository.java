0b80a3bc4e63cbc7649853715f50fd08babe2c88
enarciso2009 <enarciso2009@gmail.com>
2019-10-03 08:36:35 -0300

    Consulta com varios criterios

--- src/main/java/com/narcsoft/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/narcsoft/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.narcsoft.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -19,6 +20,10 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
 	
 	
 	List<Post> findByTitleContainingIgnoreCase(String Text); // monta uma consulta automaticamente na base de dados para localizar um texto no banco de dados...
